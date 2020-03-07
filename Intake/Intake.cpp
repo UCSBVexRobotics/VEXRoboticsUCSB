2155d5335019bfbaf2647095f3586be172e2ccb0
Alberto Huang <ahuanghe@umail.ucsb.edu>
2020-03-07 13:04:45 -0800

    Intake Class

--- Intake/Intake.cpp
+++ Intake/Intake.cpp
@@ -2,19 +2,19 @@
 
 class Intake{
   // Intake class for two motors
-  motor rightIntake, leftIntake;
   public:
-  Intake(int32_t setPort, gearSetting setRatio, bool isReversedRight, bool isReversedLeft, directionType left, directionType right);
+  Intake(int32_t setPort, gearSetting setRatio, bool isReversedRight, bool isReversedLeft, directionType one, directionType two);
   void setIntakeVelocity(double Velocity);
   double velocityInput;
+  motor rightIntake, leftIntake;
 
   private:
-  
+
 };
 
 Intake::Intake(int32_t setPort, gearSetting setRatio, bool isReversedRight,
-                bool isReversedLeft, directionType right, directionType left):
-                  // Initializing the Motors to default values
+                bool isReversedLeft, directionType one, directionType two):
+                  // Initializing the Motors
                   rightIntake(motor(setPort, setRatio, isReversedRight)),
                   leftIntake (motor(setPort, setRatio, isReversedLeft)) 
 {
@@ -22,10 +22,10 @@ Intake::Intake(int32_t setPort, gearSetting setRatio, bool isReversedRight,
   // Valid inputs for setPort: PORT + a number from 1 to 21
   // Valid Inputs for setRatio: ratio36_1, ratio18_1, ratio6_1 
   // Set isReversedRight and isReversedLeft to either true or false
-  // Set the direction of the motors (left and right) to either forward or reverse
+  // Set the direction of the motors (one and two) to either forward or reverse
 
-  rightIntake.spin(right);
-  leftIntake.spin(left);
+  rightIntake.spin(one);
+  leftIntake.spin(two);
 
 }
 
